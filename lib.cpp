066ebbd1a54ee0de6ae897faed4b4536a8f85ba4
Kojima Kensuke <k4294967296@gmail.com>
2020-08-10 19:41:35 +0900

    add fft

--- lib.cpp
+++ lib.cpp
@@ -804,3 +804,85 @@ for (int s = 0; s < 1<<n; s++) {
         if (t == 0) break;
     }
 }
+
+
+// FFT
+using C = complex<double>;
+const double PI = acos(-1);
+
+void fft(vector<C> &a, bool inv) {
+    int n = a.size();
+    if (n == 1) return;
+
+    vector<C> a0(n / 2), a1(n / 2);
+    for (int i = 0; 2 * i < n; i++) {
+        a0[i] = a[2*i];
+        a1[i] = a[2*i+1];
+    }
+    fft(a0, inv);
+    fft(a1, inv);
+
+    const double ang = 2 * PI / n * (inv ? -1 : 1);
+    const C wn(cos(ang), sin(ang));
+    C w(1);
+    for (int i = 0; 2 * i < n; i++) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n/2] = a0[i] - w * a1[i];
+        if (inv) {
+            a[i] /= 2;
+            a[i + n/2] /= 2;
+        }
+        w *= wn;
+    }
+}
+
+vector<long long> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<C> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<long long> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+
+
+// // trie
+// class Node {
+// public:
+//     int chidx[26];
+//     bool exist = false;
+//     Node() {
+//     }
+//     int child(char c) {
+//         return chidx[c - 'a'];
+//     }
+// };
+
+// class Trie {
+//     vector<Node> pool(1);
+// public:
+//     void add(string &s) {
+//         int idx = 0;
+//         for (char c : s) {
+//             int nxt = pool[idx].child(c);
+//             if (nxt == -1) {
+//                 nxt = pool.size();
+//                 pool[idx].child(c) = nxt;
+//                 pool.push_back(Node());
+//             }
+//             idx = nxt;
+//         }
+//         pool[idx].exist = true;
+//     }
+// }
