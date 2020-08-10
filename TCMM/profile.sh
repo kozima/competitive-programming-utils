93a81015245cc65ff11f3c45004d9dd551422426
Kojima Kensuke <k4294967296@gmail.com>
2020-08-10 23:38:37 +0900

    update profile.sh

--- TCMM/profile.sh
+++ TCMM/profile.sh
@@ -2,9 +2,9 @@ name=
 
 if which pprof; then
     if test ${name}.cpp -nt ${name}_prof; then
-        g++ -g --std=c++14 -lm -x c++ -O2 -o ${name}_prof -lprofiler -Wl,-no_pie ${name}.cpp
+        g++ -g --std=gnu++11 -O3 -o ${name}_prof -lprofiler -Wl,-no_pie ${name}.cpp
     fi
-    env CPUPROFILE=${name}.prof ./${name}_prof 10 -b < testcases/1.txt > /dev/null
+    env CPUPROFILE=${name}.prof ./${name}_prof 10 -b < testcases/2.txt > /dev/null
     pprof --text ${name}_prof ${name}.prof
 elif which gprof; then
     if test ! -e ${name}_prof || test ${name}.cpp -nt ${name}_prof; then
